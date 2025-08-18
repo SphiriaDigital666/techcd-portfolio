559871cc77e2beddf945dc523041b2d42f63fe76
Dominic Brian <dominic91960@gmail.com>
2025-08-18 11:54:39 +0530

    feat(contact-page): header

---
+++ client/app/(user-pages)/contact/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Header from "@/components/user/contact-page/Header";
+
+const ContactPage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default ContactPage;
