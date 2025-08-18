54e9a13ec4f827d9d6694857b3e43eddd1a80012
Dominic Brian <dominic91960@gmail.com>
2025-08-18 12:24:10 +0530

    fix(contact-page): header changes

--- client/components/user/contact-page/Header.tsx
+++ client/components/user/contact-page/Header.tsx
@@ -14,11 +14,32 @@ const Header = () => {
           Contact Us
         </p>
 
-        <div className="mt-[1em] flex flex-col gap-[0.5em] lg:mt-[0.5em] lg:flex-row lg:gap-[1.5em]">
-          <h2 className="dark:from-foreground to-foreground bg-gradient-to-b from-[#999999] bg-clip-text text-[26px] font-semibold text-transparent uppercase sm:text-[34px] md:text-[43px] lg:shrink-0 lg:text-[51px] xl:text-[60px] 2xl:text-[68px] dark:to-[#999999]">
-            Reach Out for <br /> More Information
-          </h2>
-          <p className="text-foreground/40 mx-auto max-w-9/10 text-center text-[12px] uppercase sm:text-[13px] md:text-[15px] lg:mx-0 lg:mt-[0.5em] lg:max-w-none lg:text-left lg:text-[16px] xl:text-[18px] 2xl:text-[20px]">
+        <div className="mt-[1em] grid grid-cols-1 gap-[1.5em] lg:mt-[0.5em] lg:grid-cols-11 lg:gap-0">
+          <div className="lg:col-span-6">
+            <h2 className="dark:from-foreground to-foreground bg-gradient-to-b from-[#999999] bg-clip-text text-[26px] font-semibold text-transparent uppercase sm:text-[34px] md:text-[43px] lg:shrink-0 lg:text-[51px] xl:text-[60px] 2xl:text-[68px] dark:to-[#999999]">
+              Reach Out for <br /> More Information
+            </h2>
+
+            <div className="mt-[0.5em] flex justify-center gap-[1em] lg:justify-normal">
+              <button className="rounded-full border p-[0.5em] opacity-80 hover:opacity-100">
+                <FaFacebookF />
+              </button>
+
+              <button className="rounded-full border p-[0.5em] opacity-80 hover:opacity-100">
+                <FaYoutube />
+              </button>
+
+              <button className="rounded-full border p-[0.5em] opacity-80 hover:opacity-100">
+                <FaTiktok />
+              </button>
+
+              <button className="rounded-full border p-[0.5em] opacity-80 hover:opacity-100">
+                <FaInstagram />
+              </button>
+            </div>
+          </div>
+
+          <p className="text-foreground/40 mx-auto max-w-9/10 text-center text-[12px] uppercase sm:text-[13px] md:text-[15px] lg:col-span-5 lg:mx-0 lg:mt-[0.5em] lg:max-w-none lg:text-left lg:text-[16px] xl:text-[18px] 2xl:text-[20px]">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry&apos;s standard dummy
             text ever since the 1500s, when an unknown printer took a galley of
@@ -26,24 +47,6 @@ const Header = () => {
             not only five centuries, but also the leap into electronic
             typesetting, remaining essentially unchanged.,
           </p>
-        </div>
-
-        <div className="flex gap-[1em]">
-          <button className="rounded-full border p-[0.5em] opacity-80 hover:opacity-100">
-            <FaFacebookF />
-          </button>
-
-          <button className="rounded-full border p-[0.5em] opacity-80 hover:opacity-100">
-            <FaYoutube />
-          </button>
-
-          <button className="rounded-full border p-[0.5em] opacity-80 hover:opacity-100">
-            <FaTiktok />
-          </button>
-
-          <button className="rounded-full border p-[0.5em] opacity-80 hover:opacity-100">
-            <FaInstagram />
-          </button>
         </div>
       </div>
     </section>
