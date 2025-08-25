5c1e251df2c2d6bd61e45f9cedbfeffe140fa5eb
Dominic Brian <dominic91960@gmail.com>
2025-08-25 14:42:51 +0530

    feat(server): new server with user role apis

---
+++ server/routes/user-role.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+const userRoleController = require("../controllers/user-role");
+const {
+  validateCreateUserRole,
+  validateUpdateUserRole,
+  validateIdParam,
+} = require("../validators/user-role-validator");
+const { runValidation } = require("../middleware/validate");
+
+router.post(
+  "/",
+  validateCreateUserRole,
+  runValidation,
+  userRoleController.createUserRole
+);
+
+router.get("/", userRoleController.getUserRoles);
+
+router.get(
+  "/:id",
+  validateIdParam,
+  runValidation,
+  userRoleController.getUserRoleById
+);
+
+router.patch(
+  "/:id",
+  validateUpdateUserRole,
+  runValidation,
+  userRoleController.updateUserRole
+);
+
+router.delete(
+  "/:id",
+  validateIdParam,
+  runValidation,
+  userRoleController.deleteUserRole
+);
+
+module.exports = router;
